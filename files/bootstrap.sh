3135de2f23f9397fac13f98a9bfec0bbc320f851
Yann Hodique <hodiquey@vmware.com>
2015-06-07 16:39:34 -0700

    get pip from configurable URL

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -8,33 +8,33 @@ if [[ -e $PKG_HOME/.bootstrapped ]]; then
   exit 0
 fi
 
-mkdir -p `dirname $PYPY_HOME`
-wget -O - https://bitbucket.org/pypy/pypy/downloads/pypy-$PYPY_VERSION-linux64.tar.bz2 |tar -xjf -
-mv -n pypy-$PYPY_VERSION-linux64 $PYPY_HOME
+mkdir -p `dirname "$PYPY_HOME"`
+wget -O - "$PYPY_DOWNLOAD_URL-$PYPY_VERSION-linux64.tar.bz2" |tar -xjf -
+mv -n "pypy-$PYPY_VERSION-linux64" "$PYPY_HOME"
 
 ## library fixup
-mkdir -p $PYPY_HOME/lib
+mkdir -p "$PYPY_HOME/lib"
 
 CURSES_LIB=/lib64/libncurses.so.5.9
 if [ -e "$CURSES_LIB" ]; then
-    ln -snf $CURSES_LIB $PYPY_HOME/lib/libtinfo.so.5
+    ln -snf "$CURSES_LIB" "$PYPY_HOME/lib/libtinfo.so.5"
 fi
 
-mkdir -p $PKG_HOME/bin
+mkdir -p "$PKG_HOME/bin"
 
-cat > $PKG_HOME/bin/python <<EOF
+cat > "$PKG_HOME/bin/python" <<EOF
 #!/bin/bash
-LD_LIBRARY_PATH=$PYPY_HOME/lib:\$LD_LIBRARY_PATH exec $PYPY_HOME/bin/pypy "\$@"
+LD_LIBRARY_PATH="$PYPY_HOME/lib:\$LD_LIBRARY_PATH" exec "$PYPY_HOME/bin/pypy" "\$@"
 EOF
 
-cat > $PKG_HOME/bin/pip <<EOF
+cat > "$PKG_HOME/bin/pip" <<EOF
 #!/bin/bash
-LD_LIBRARY_PATH=$PYPY_HOME/lib:\$LD_LIBRARY_PATH exec $PYPY_HOME/bin/pip "\$@"
+LD_LIBRARY_PATH="$PYPY_HOME/lib:\$LD_LIBRARY_PATH" exec "$PYPY_HOME/bin/pip" "\$@"
 EOF
 
-chmod +x $PKG_HOME/bin/python
-chmod +x $PKG_HOME/bin/pip
+chmod +x "$PKG_HOME/bin/python"
+chmod +x "$PKG_HOME/bin/pip"
 
-$PKG_HOME/bin/python --version
+"$PKG_HOME/bin/python" --version
 
-touch $PKG_HOME/.bootstrapped
+touch "$PKG_HOME/.bootstrapped"
