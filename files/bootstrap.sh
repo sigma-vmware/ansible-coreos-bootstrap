3224c7d53728fa28a567e4a06d98a706b7e4ab58
Yann Hodique <hodiquey@vmware.com>
2015-11-26 10:00:53 -0800

    move ssl logic into the bootstrap script

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -23,7 +23,13 @@ FACTSD="/etc/ansible/facts.d"
 sudo mkdir -p "$FACTSD"
 sudo chown core "$FACTSD"
 
+PYPY_SSL_PATH=`$PYPY_INSTALL/bin/pypy -c 'from __future__ import print_function; import ssl; print(ssl.get_default_verify_paths().openssl_capath)'`
+
+sudo mkdir -p `dirname $PYPY_SSL_PATH`
+sudo ln -s $COREOS_SSL_CERTS $PYPY_SSL_PATH
+
 cat > "$FACTSD/bootstrap.fact" <<EOF
 [pypy]
 version=$PYPY_VERSION
+ssl_path=$PYPY_SSL_PATH
 EOF
