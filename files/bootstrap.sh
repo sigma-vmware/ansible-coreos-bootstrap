dea76c5bcff68f694ecfa60de8133a7d780b6d0b
Yann Hodique <hodiquey@vmware.com>
2015-11-26 10:42:06 -0800

    better handling of permissions

--- files/bootstrap.sh
+++ files/bootstrap.sh
@@ -3,21 +3,31 @@
 set -e
 set -x
 
-cd
+if [[ `stat -c '%U' $PKG_HOME` != `whoami` ]]; then
+    SUDO="sudo"
+else
+    SUDO=""
+fi
 
-mkdir -p `dirname "$PYPY_HOME"`
+PYPY_HOME="$PKG_HOME/pypy"
+PYPY_INSTALL="$PKG_HOME/.pypy"
+
+cd /tmp
+
 wget -O - "$PYPY_DOWNLOAD_URL/pypy-$PYPY_VERSION-$PYPY_FLAVOR.tar.bz2" |tar -xjf -
 
-rm -rf "$PYPY_INSTALL"
-mv -n "pypy-$PYPY_VERSION-$PYPY_FLAVOR" "$PYPY_INSTALL"
+$SUDO rm -rf "$PYPY_INSTALL"
+$SUDO mv -n "pypy-$PYPY_VERSION-$PYPY_FLAVOR" "$PYPY_INSTALL"
 
-rm -rf "$PYPY_HOME"
-"$PYPY_INSTALL/bin/pypy" "$PYPY_INSTALL/bin/virtualenv-pypy" "$PYPY_HOME"
+$SUDO mkdir -p `dirname "$PYPY_HOME"`
+$SUDO rm -rf "$PYPY_HOME"
 
-mkdir -p "$PKG_HOME/bin"
+$SUDO "$PYPY_INSTALL/bin/pypy" "$PYPY_INSTALL/bin/virtualenv-pypy" "$PYPY_HOME"
 
-ln -sf "$PYPY_HOME/bin/python" "$PKG_HOME/bin/python"
-ln -sf "$PYPY_HOME/bin/pip" "$PKG_HOME/bin/pip"
+$SUDO mkdir -p "$PKG_HOME/bin"
+
+$SUDO ln -sf "$PYPY_HOME/bin/python" "$PKG_HOME/bin/python"
+$SUDO ln -sf "$PYPY_HOME/bin/pip" "$PKG_HOME/bin/pip"
 
 sudo mkdir -p "$ANSIBLE_FACTS_DIR"
 sudo chown `whoami` "$ANSIBLE_FACTS_DIR"
@@ -25,7 +35,7 @@ sudo chown `whoami` "$ANSIBLE_FACTS_DIR"
 PYPY_SSL_PATH=`$PYPY_INSTALL/bin/pypy -c 'from __future__ import print_function; import ssl; print(ssl.get_default_verify_paths().openssl_capath)'`
 
 sudo mkdir -p `dirname $PYPY_SSL_PATH`
-sudo ln -s $COREOS_SSL_CERTS $PYPY_SSL_PATH
+sudo ln -sf $COREOS_SSL_CERTS $PYPY_SSL_PATH
 
 PIP_VERSION=`$PYPY_HOME/bin/pip --version | awk '{ print $2 }'`
 WHEEL_VERSION=`$PYPY_HOME/bin/wheel version | awk '{ print $2 }'`
